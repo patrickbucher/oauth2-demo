2af53403886e0cfbc43273b43c73661eedec2dc1
christopherchristensen <christopher.christensen@live.com>
2019-04-29 09:32:54 +0200

    fix merge conflict

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-
-cd resource && go run main.go &
-cd client && go run main.go &
-cd authserver && go run main.go
-=======
 #!/bin/sh
 
 ./kill.sh
@@ -17,4 +10,3 @@ go run resource.go &
 
 cd ../client
 go run client.go &
->>>>>>> e0e06c912bece7a2e02a9ff0d82c80a9d6ba6d92
