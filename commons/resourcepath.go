e1653ae4bc076e1261c23bb1b1baf82b4290af0b
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-05-04 21:36:04 +0200

    added function to extract nth path element to commons

---
+++ commons/resourcepath.go
@@ -0,0 +1,20 @@
+package commons
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ExtractPathElement extracts the nthElement (zero-based) element of the
+// resource path and returns it without surrounding path delimiters ("/").
+// An error is returnes if the operation fails.
+// For resource "/foo/bar/qux" and nThElement 1, "bar" is returned.
+func ExtractPathElement(resource string, nthElement uint) (string, error) {
+	paths := strings.Split(resource, "/") // ["", "foo", "bar", "qux"]
+	paths = paths[1:]                     // cut off starting element (always empty)
+	if len(paths) <= nthElement {
+		return "", fmt.Errorf("resource path '%s' too short to extract %d elements",
+			resource, nthElement)
+	}
+	return paths[nthElement], nil
+}
