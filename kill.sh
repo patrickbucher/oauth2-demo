74d71f508950c330f400e674f79a76bc94a91d87
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-05-04 21:37:43 +0200

    make sure to grap correct PID when killing processes, not empty string

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps ax | grep 'go' | grep -v 'vim' | grep -e 'authserver\|resource\|client' | cut -d' ' -f1 | while read pid
+ps ax | grep 'go' | grep -v 'vim' | grep -e 'authserver\|resource\|client' | awk '{print $1}' | while read pid
 do
     echo "kill $pid"
     kill $pid 2>/dev/null
